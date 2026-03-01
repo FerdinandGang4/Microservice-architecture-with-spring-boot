79a1610d73ef88fd724358203a956848eba27e27
Gang Ferdinand Dinga <ferdinandgang4@gmail.com>
2026-03-01 13:10:04 -0600

    Implement stock service controller

---
+++ product_and_stuctmicroservice/StockServicev1/StockServicev1/src/main/java/com/example/StockServicev1/Controller/StockController.java
@@ -0,0 +1,32 @@
+package com.example.StockServicev1.Controller;
+
+import com.example.StockServicev1.Dto.StockResponseDto;
+import com.example.StockServicev1.Service.StockService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/stocks")
+public class StockController {
+
+    private final StockService stockService;
+
+    public StockController(StockService stockService) {
+        this.stockService = stockService;
+    }
+
+    @GetMapping("/{productNumber}")
+    public ResponseEntity<StockResponseDto> getStock(
+            @PathVariable String productNumber) {
+
+        int stock = stockService.getStockByProductNumber(productNumber);
+
+        StockResponseDto response =
+                new StockResponseDto(productNumber, stock);
+
+        return ResponseEntity.ok(response);
+    }
+}
--- product_and_stuctmicroservice/StockServicev1/StockServicev1/src/main/java/com/example/StockServicev1/Service/StockServiceImpl.java
+++ product_and_stuctmicroservice/StockServicev1/StockServicev1/src/main/java/com/example/StockServicev1/Service/StockServiceImpl.java
@@ -1,4 +1,16 @@
 package com.example.StockServicev1.Service;
 
-public class StockServiceImpl {
+import org.springframework.stereotype.Service;
+
+@Service
+public class StockServiceImpl implements StockService{
+    @Override
+    public int getStockByProductNumber(String productNumber) {
+        return switch (productNumber) {
+            case "P1001" -> 10;
+            case "P1002" -> 25;
+            case "P1003" -> 0;
+            default -> 0;
+        };
+    }
 }
