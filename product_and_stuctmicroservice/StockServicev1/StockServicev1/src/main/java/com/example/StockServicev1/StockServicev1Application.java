2ae11614e9b5aa533accba4565668888cd55becc
Gang Ferdinand Dinga <ferdinandgang4@gmail.com>
2026-03-01 09:11:21 -0600

    Initial commite with startup project

---
+++ product_and_stuctmicroservice/StockServicev1/StockServicev1/src/main/java/com/example/StockServicev1/StockServicev1Application.java
@@ -0,0 +1,13 @@
+package com.example.StockServicev1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockServicev1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockServicev1Application.class, args);
+	}
+
+}
---
+++ product_and_stuctmicroservice/_ProductServicev1/ProductServicev1/src/main/java/com/example/ProductServicev1/ProductServicev1Application.java
@@ -0,0 +1,13 @@
+package com.example.ProductServicev1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductServicev1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductServicev1Application.class, args);
+	}
+
+}
