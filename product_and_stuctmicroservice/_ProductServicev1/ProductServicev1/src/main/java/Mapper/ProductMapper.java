e549b9543a68d7c62b6c5b03128a3cf0a8ac87ac
Gang Ferdinand Dinga <ferdinandgang4@gmail.com>
2026-03-01 11:06:30 -0600

    Implement mapper class todot method

--- product_and_stuctmicroservice/_ProductServicev1/ProductServicev1/src/main/java/Mapper/ProductMapper.java
+++ product_and_stuctmicroservice/_ProductServicev1/ProductServicev1/src/main/java/Mapper/ProductMapper.java
@@ -1,4 +1,17 @@
 package Mapper;
 
+import com.example.ProductServicev1.Dtos.ProductRequestDto;
+import com.example.ProductServicev1.Dtos.ProductResponseDto;
+import com.example.ProductServicev1.Model.Product;
+
 public class ProductMapper {
+    public ProductResponseDto toDto(Product product)
+    {
+        ProductResponseDto productResponseDto = new ProductResponseDto();
+        productResponseDto.setProductName(product.getProductName());
+        productResponseDto.setProductNumber(product.getProductNumber());
+        productResponseDto.setNumberOnStock(product.getNumberOnStock());
+
+        return productResponseDto;
+    }
 }
--- product_and_stuctmicroservice/_ProductServicev1/ProductServicev1/src/main/java/com/example/ProductServicev1/Dtos/ProductResponseDto.java
+++ product_and_stuctmicroservice/_ProductServicev1/ProductServicev1/src/main/java/com/example/ProductServicev1/Dtos/ProductResponseDto.java
@@ -1,5 +1,10 @@
 package com.example.ProductServicev1.Dtos;
 
+import lombok.Data;
+import org.springframework.stereotype.Component;
+
+@Component
+@Data
 public class ProductResponseDto {
     private String productNumber;
     private String productName;
