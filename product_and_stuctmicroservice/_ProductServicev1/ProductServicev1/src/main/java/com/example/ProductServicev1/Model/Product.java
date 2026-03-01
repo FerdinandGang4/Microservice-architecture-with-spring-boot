ae0e720a079f819311e924533037369cbc7b9991
Gang Ferdinand Dinga <ferdinandgang4@gmail.com>
2026-03-01 10:10:53 -0600

    establish connection with database and connection with mongo compass for view database

--- product_and_stuctmicroservice/_ProductServicev1/ProductServicev1/src/main/java/com/example/ProductServicev1/Model/Product.java
+++ product_and_stuctmicroservice/_ProductServicev1/ProductServicev1/src/main/java/com/example/ProductServicev1/Model/Product.java
@@ -1,5 +1,19 @@
 package com.example.ProductServicev1.Model;
 
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection = "products")
 public class Product {
+    @Id
+    private String productNumber;
+    private String productName;
+    private int numberOnStock;
 }
