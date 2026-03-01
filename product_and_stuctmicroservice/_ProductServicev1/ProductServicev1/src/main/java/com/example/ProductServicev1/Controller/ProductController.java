8f7bed90ccb789a40642c3b4ed389ceb2160df67
Gang Ferdinand Dinga <ferdinandgang4@gmail.com>
2026-03-01 12:05:37 -0600

    Implement controller method get all products method

--- product_and_stuctmicroservice/_ProductServicev1/ProductServicev1/src/main/java/com/example/ProductServicev1/Controller/ProductController.java
+++ product_and_stuctmicroservice/_ProductServicev1/ProductServicev1/src/main/java/com/example/ProductServicev1/Controller/ProductController.java
@@ -8,10 +8,10 @@ import com.example.ProductServicev1.Service.ProductService;
 import com.example.ProductServicev1.Service.ProductServiceImp;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("api/products")
@@ -33,4 +33,24 @@ public class ProductController {
 
         return ResponseEntity.status(HttpStatus.CREATED).body(prdto);
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductResponseDto> getProduct(@PathVariable String id)
+    {
+        Product product = productService.getProductById(id);
+        return ResponseEntity.status(HttpStatus.OK).body(productMapper.toDto(product));
+    }
+
+    public ResponseEntity<List<ProductResponseDto>> getProducts()
+    {
+        List<Product> products = productService.getAllProducts();
+        List<ProductResponseDto> dtos = new ArrayList<>();
+        for(Product p : products)
+        {
+           var res =  productMapper.toDto(p);
+            dtos.add(res);
+        }
+        return ResponseEntity.ok().body(dtos);
+    }
+
 }
